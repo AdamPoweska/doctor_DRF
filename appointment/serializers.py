a665e58a3fa9f6d2316603fe40d5756b660d0c85
AdamPoweska <adam.poweska@gmail.com>
2025-03-05 23:19:19 +0100

    doctor names as pk's and __str__

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -11,7 +11,8 @@ class DoctorTypeSerializer(serializers.ModelSerializer):
 
 
 class DoctorNameSerializer(serializers.ModelSerializer):
-    main_specialization = DoctorTypeSerializer()
+    main_specialization = serializers.PrimaryKeyRelatedField(queryset=DoctorType.objects.all()) # ściągamy pk z querysetu 
+    specialization_name = serializers.CharField(source="main_specialization.__str__", read_only=True)  # dodajemy pole tylko do odczytu, które poda __str__ z modelu - w ten sposób będzie widać nazwisko lekarza a nie tylko jego numer
 
     class Meta:
         model = DoctorName
@@ -19,26 +20,34 @@ class DoctorNameSerializer(serializers.ModelSerializer):
 
 
 class AppointmentDateseSerializer(serializers.ModelSerializer):
-    doctor = DoctorNameSerializer()
+    doctor = serializers.PrimaryKeyRelatedField(queryset=DoctorName.objects.all())
+    doctor_details = serializers.CharField(source="doctor.__str__", read_only=True)
 
     class Meta:
         model = AppointmentDates
         fields = '__all__'
 
 
-class FinalAppointmentDetailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FinalAppointmentDetails
-        fields = '__all__'
-
-# poniższe do sprawdzenia jak się będą sprawowały:
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+
+
+
+
+# poniższe przy dodawaniu użytkowników
+# class FinalAppointmentDetailsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = FinalAppointmentDetails
+#         fields = '__all__'
+
+# # poniższe do sprawdzenia jak się będą sprawowały:
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['url', 'username', 'email', 'groups']
+
+
+# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ['url', 'name']
