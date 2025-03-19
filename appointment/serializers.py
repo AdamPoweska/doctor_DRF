c4342f6f8492baa6631f8c4840082db6e23ff49f
AdamPoweska <adam.poweska@gmail.com>
2025-03-19 22:06:37 +0100

    removed doubled nested serializers

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -5,21 +5,23 @@ from django.contrib.auth.models import User, Group
 from .models import *
 
 
-class DoctorTypeSerializer(serializers.ModelSerializer): #HyperlinkedModelSerializer - dodaje linki, żeby można było wchodzić w opcje: delete, patch, put poprzez link a nie poprzez wpisywanie w przeglądarkę, jest też bardziej zgodne z filozofią REST: "HATEOAS – Hypermedia as the Engine of Application State"
+class DoctorTypeSerializer(serializers.ModelSerializer):
+    # sprawdzenie przed zapisem przy użyciu validators, czy "specialization" jest unique dla queryset'u "DoctorType"
     specialization = serializers.CharField(
         max_length=50,
-        validators=[UniqueValidator(queryset=DoctorType.objects.all())] # sprawdzenie przed zapisem przy użyciu validators
+        validators=[UniqueValidator(queryset=DoctorType.objects.all())]
     )
     
-    doctors = serializers.SerializerMethodField
+    # zamiast zwykłego pola będzie wywołana metoda 'get_doctors' wg wzorca 'get_{field_name}'
+    doctors = serializers.SerializerMethodField()
 
     class Meta:
         model = DoctorType
-        fields = ['id', 'specialization']
+        fields = ['id', 'specialization', 'doctors']
     
-    def get_doctors(self, obj):
-        doctors = DoctorName.objects.filter(main_specialization=obj)
-        return DoctorNameSimpleSerializer(doctors, many=True).data
+    def get_doctors(self, obj): # 'obj' jest przekazywany automatycznie przez DRF przy serializowaniu obiektu
+        doctors = DoctorName.objects.filter(main_specialization=obj) # filtracja lekarzy po obj
+        return DoctorNameSimpleSerializer(doctors, many=True).data # zwrócenie danych opakowanych w klasę 'DoctorNameSimpleSerializer' --> klasa jest zaraz poniżej w kodzie
 
 
 class DoctorNameSimpleSerializer(serializers.ModelSerializer):
@@ -29,16 +31,37 @@ class DoctorNameSimpleSerializer(serializers.ModelSerializer):
 
 
 class DoctorNameDetailSerializer(serializers.ModelSerializer):
-    appointments = serializers.SerializerMethodField()
+    appointments = serializers.SerializerMethodField() # analogiczny mechanizm jak w klasie 'DoctorTypeSerializer' <-- 'appointments' zostanie wyołana funkcja 'get_appointments'
 
     class Meta:
         model = DoctorName
         fields = ['id', 'first_name', 'last_name', 'main_specialization', 'appointments']
+        # fields = ['id', 'first_name', 'last_name', 'main_specialization']
     
     def get_appointments(self, obj):
         visits = AppointmentDates.objects.filter(doctor=obj)
         return AppointmentDatesSerializer(visits, many=True).data
  
+
+class AppointmentDatesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppointmentDates
+        fields = ['id', 'date', 'time']
+
+
+class DoctorNameNestedSerializer(serializers.ModelSerializer):
+    appointments = AppointmentDatesSerializer(source='appointmentdates_set', many=True, read_only=True)
+
+    class Meta:
+        model = DoctorName
+        fields = ['id', 'first_name', 'last_name', 'appointments']
+
+
+class DoctorTypeSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DoctorType
+        fields = ['id', 'specialization']
+
 
 class DoctorNameSerializer(serializers.ModelSerializer):
     # mogę użyć poniższego kodu żeby wyciągnąć nazwę specjalizacji tak żeby nie widniała tylko jako numer
@@ -49,7 +72,7 @@ class DoctorNameSerializer(serializers.ModelSerializer):
     # main_specialization = serializers.SlugRelatedField(queryset=DoctorType.objects.all(), slug_field='specialization')
     
     # Jednak rezygnuje z powyższego, żeby zrobic nested serializer, ktory i tak wszystko wyswietli
-    main_specialization = DoctorTypeSerializer(read_only=True) 
+    main_specialization = DoctorTypeSimpleSerializer(read_only=True) 
     
     # kod tylko do zapisu - przy nested nie pojawi nam sie dropdown lista
     main_specialization_id = serializers.PrimaryKeyRelatedField(
@@ -62,6 +85,7 @@ class DoctorNameSerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorName
         fields = ['id', 'first_name', 'last_name', 'main_specialization', 'main_specialization_id']
+        # fields = ['id', 'first_name', 'last_name', 'main_specialization']
         # UniqueTogetherValidator - sprawdzamy czy kombinacja danych pól już występuje
         validators = [UniqueTogetherValidator(
             queryset=DoctorName.objects.all(),
@@ -90,22 +114,6 @@ class DoctorNameSerializer(serializers.ModelSerializer):
 #             queryset=AppointmentDates.objects.all(),
 #             fields=['doctor', 'date', 'time']
 #         )]
-
-class AppointmentDatesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AppointmentDates
-        fields = ['id', 'date', 'time']
-
-
-class DoctorNameNestedSerializer(serializers.ModelSerializer):
-    appointments = AppointmentDatesSerializer(source='appointmentdates_set', many=True, read_only=True)
-
-    class Meta:
-        model = DoctorName
-        fields = ['id', 'first_name', 'last_name', 'appointments']
-
-
-
 
 
 # poniższe przy dodawaniu użytkowników - z tutoriala
