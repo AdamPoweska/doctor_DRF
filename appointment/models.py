c27e93a3b5b8e0ee18ce63d0564826f3424e2ef2
Adam <166754975+AdamPoweska@users.noreply.github.com>
2025-08-31 21:23:41 +0200

    Update models.py

--- appointment/models.py
+++ appointment/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class DoctorType(models.Model):
     specialization = models.CharField(
         max_length=50,
-        unique=True # unique na poziomie bazy danych
+        unique=True
     )
 
     def __str__(self):
@@ -18,7 +18,7 @@ class DoctorName(models.Model):
     main_specialization = models.ForeignKey(DoctorType, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.main_specialization.specialization})" # "main_specialization" to obiekt DoctorType wiec __str__ zwróci np. "object (3)"
+        return f"{self.first_name} {self.last_name} ({self.main_specialization.specialization})"
 
 
 class AppointmentDates(models.Model):
