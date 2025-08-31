041b8146fe8712eb76fda7069fc49a34c594c74c
Adam <166754975+AdamPoweska@users.noreply.github.com>
2025-08-31 21:27:17 +0200

    Update views.py

--- appointment/views.py
+++ appointment/views.py
@@ -10,26 +10,11 @@ from .serializers import *
 from .models import *
 
 
-# class HomeView(TemplateView):
-#     template_name = 'hello.html'
-
-
-# class HomeLoginView(LoginRequiredMixin, TemplateView):
-#     template_name = 'hellologin.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["doctors"] = DoctorName.objects.all()
-#         context["specializations"] = DoctorType.objects.all()
-#         context["appointments"] = AppointmentDates.objects.all()
-#         return context
-
-
 class IsReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS: # safe_methods to GET', 'HEAD', 'OPTIONS' - czyli do tego moga miec dostep wszycy
+        if request.method in permissions.SAFE_METHODS: 
             return True
-        elif request.user and request.user.is_staff: # jesli nie sa to safe methods ale uzytkownik jest zalogowany i jest adminem
+        elif request.user and request.user.is_staff: 
             return True
         
 
@@ -39,7 +24,7 @@ class AppointmentDateCrudViewSet(viewsets.ModelViewSet):
     """
     queryset = AppointmentDates.objects.all()
     serializer_class = AppointmentDatesSerializer
-    permission_classes = [permissions.IsAuthenticated] # tylko zalogowani będą mieć dostęp (możesz też zmienić w settings dla całego projektu)
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class DoctorNameCrudViewSet(viewsets.ModelViewSet):
@@ -51,48 +36,16 @@ class DoctorNameCrudViewSet(viewsets.ModelViewSet):
     permission_classes = [IsReadOnly]
 
 
-# class DoctorTypeCrudViewSet(viewsets.ModelViewSet):
-#     """
-#     (C)reate (R)ead (U)date (D)elete actions.
-#     """
-#     queryset = DoctorType.objects.all()
-#     serializer_class = DoctorTypeSerializer
-#     permis_classes = [IsReadOnly]
-
-
 class DoctorTypeViewSet(viewsets.ModelViewSet):
     queryset = DoctorType.objects.all()
     serializer_class = DoctorTypeSerializer
     permission_classes = [IsReadOnly]
-
-    # /doctor_types/<id>/doctors/
-    # @action(detail=True, methods=['get'])
-    # def doctors(self, request, pk=None):
-    #     specialization = self.get_object()
-    #     doctors = DoctorName.objects.filter(main_specialization=specialization)
-    #     serializer = DoctorNameSimpleSerializer(doctors, many=True, context={'request': request})
-    #     return Response(serializer.data)
-
-
-# class DoctorNameNestedViewSet(viewsets.ViewSet):
-#     permission_classes = [IsReadOnly]
-
-#     # /doctor_types/<doctor_type_pk>/doctors/<doctor_pk>/
-#     def retrieve(self, request, doctor_type_pk=None, pk=None):
-#         try:
-#             doctor = DoctorName.objects.get(pk=pk, main_specialization__id=doctor_type_pk)
-#         except DoctorName.DoesNotExist:
-#             return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-#         serializer = DoctorNameDetailSerializer(doctor, context={'request': request})
-#         return Response(serializer.data)
 
 
 class DoctorNameNestedViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = DoctorNameNestedSerializer
 
     def get_queryset(self):
-        # Pobieramy tylko lekarzy dla konkretnej specjalizacji
         return DoctorName.objects.filter(main_specialization_id=self.kwargs['doctor_type_pk'])
 
 
@@ -106,16 +59,3 @@ class AppointmentNestedViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         doctor = DoctorName.objects.get(pk=self.kwargs['doctor_pk'])
         serializer.save(doctor=doctor)
-
-        
-# Na daną chwilę nie jest nam potrzebne, przyda się jednak w przyszłości - widok na read only
-# class DataViewSet(viewsets.ReadOnlyModelViewSet):
-#     """
-#     Read only view.
-#     """
-#     queryset = DoctorType.objects.all()
-#     serializer_class = DoctorTypeSerializer
-
-
-
-
